821b87ac3faea57689aec59d408708e70ae9145b
Leonardo <devleeors@gmail.com>
2023-11-04 23:34:59 -0300

    adjust cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,9 @@ const uploadConfig = require("./configs/uploadConfig");
 
 // app use
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://food-explorer-3.netlify.app", credentials: true })
+);
 app.use(express.json());
 app.use(router);
 
