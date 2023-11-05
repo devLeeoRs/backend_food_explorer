e0970d543c074da4d1316784daf3baf769801ac3
Leonardo <devleeors@gmail.com>
2023-11-04 21:33:14 -0300

    config pm2

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,12 +2,15 @@ const { Router } = require("express");
 const UsersController = require("../controllers/Users.Controller");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const roleAuthenticated = require("../middlewares/roleAuthenticated");
+const UsersValidatedController = require("../controllers/UsersValidated.Controller");
 
 const userRoute = Router();
 
 const usersController = new UsersController();
+const userValidatedController = new UsersValidatedController();
 
 userRoute.post("/", usersController.create);
+userRoute.get("/validated", ensureAuthenticated, userValidatedController.index);
 userRoute.put("/", ensureAuthenticated, usersController.update);
 userRoute.delete(
   "/:id",
