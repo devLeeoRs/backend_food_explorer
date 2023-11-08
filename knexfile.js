809d76a4b16a335a374d7d2743dcf2fd9ed534b9
Leonardo <devleeors@gmail.com>
2023-11-08 20:00:44 -0300

    edit knex

--- knexfile.js
+++ knexfile.js
@@ -21,25 +21,3 @@ module.exports = {
     useNullAsDefault: true,
   },
 };
-
-// module.exports = {
-//   development: {
-//     client: "pg",
-//     connection: {
-//       database: "food_db",
-//       user: "postgres",
-//       password: "91617915",
-//       host: "localhost",
-//       port: 5434,
-//     },
-//     migrations: {
-//       directory: path.resolve(
-//         __dirname,
-//         "src",
-//         "database",
-//         "knex",
-//         "migrations"
-//       ),
-//     },
-//   },
-// };
